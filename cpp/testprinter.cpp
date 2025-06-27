42665d3d6655ed38a47c0ba0c456aa038121b1be
moka <mohamedibrahim10102010@gmail.com>
2025-06-27 18:28:39 +0300

    adding the new codes + the print

---
+++ cpp/testprinter.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+#include"print.hpp"
+using namespace std;
+
+
+
+
+
+int main()
+{
+
+    string name = "moahmed";
+    int age = 14;
+    print("hi ", name , " age : " , age);
+}
